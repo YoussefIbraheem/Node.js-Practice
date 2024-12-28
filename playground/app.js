88c7c66577540f5bf4b21c6d33898ddc1b467975
YoussefIbraheem <youssefm.muhammed@gmail.com>
2024-12-28 18:53:27 +0200

    playground reform

--- playground/app.js
+++ playground/app.js
@@ -18,4 +18,8 @@ app.use((req, res, next) => {
     res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
-app.listen(3000);
+const PORT = 3000;
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on port http://localhost:${PORT}`);
+});
